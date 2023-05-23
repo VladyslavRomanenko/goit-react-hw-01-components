1a3606eb9d805831358fc45565a9534bf598bf5e
Vladyslav <vladyslavromanenkoo@gmail.com>
2023-05-23 10:56:53 +0300

    1-2 task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
+import { Profile } from "../index";
+import { Statistics } from "../index";
+import user from '../json/user.json'
+import data from '../json/data.json'
+import transactions from '../json/transactions.json'
+import friends from '../json/friends.json'
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
     </div>
-  );
-};
+  )
+}
+ export default App;
